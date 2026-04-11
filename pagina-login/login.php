738466b847b9ca866dc7c88ea8b5bcc799798d45
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-04-11 12:37:45 -0300

    pagina de login

---
+++ pagina-login/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <section>
+            <form action="valida.php" method="POST">
+                <input type="email" name="email" id="iemail" placeholder="Digite seu e-mail de usuário">
+                <input type="password" name="senha" id="isenha" placeholder="Digite sua senha">
+                <input type="submit" value="Log in">
+            </form>
+        </section>
+    </main>
+</body>
+</html>
